b0a1a9b5a6bbd5d6b4581a4f41a811d1f9965aab
Maxime <maxime.lorne.pro@gmail.com>
2024-05-12 20:13:54 +0200

    Modification de l'affichage de la navbar. Ajout du bouton

--- Laravel_Occilan5/resources/views/navbar/navbar.blade.php
+++ Laravel_Occilan5/resources/views/navbar/navbar.blade.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+      {{-- <a class="navbar-brand" href="#">{{config('app.name')}}</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </button> --}}
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav nav-underline">
           <li class="nav-item">
             <a class="nav-link @if(Request::route()->getName() == 'app_home') active @endif" aria-current="page" href="{{route('app_home')}}">Home</a>
           </li>
@@ -15,7 +15,11 @@
 
         </ul>
 
+      {{-- </div> --}}
+      <div class="btn-group ms-auto">
+        <button type="button" class="btn btn-danger">Login</button>
+        </button>
       </div>
-
     </div>
   </nav>
+
